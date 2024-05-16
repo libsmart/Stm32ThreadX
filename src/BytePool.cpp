57c918b35f3e40ad5dce07a26d93dbad607206da
Roland Rusch <roland@rusch.lu>
2024-05-16 22:52:29 +0200

    #2 Make TX_BYTE_POOL a collection in BytePool

--- src/BytePool.cpp
+++ src/BytePool.cpp
@@ -15,7 +15,7 @@ UINT BytePool::create(VOID *pool_start, ULONG pool_size) {
     CHAR bytePool_name[] = "Stm32ThreadX::BytePool";
 
     // Create the IP instance
-    ret = tx_byte_pool_create(this, bytePool_name, pool_start, pool_size);
+    ret = tx_byte_pool_create(bytePool, bytePool_name, pool_start, pool_size);
     if (ret != TX_SUCCESS) {
         log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)
                 ->printf("Byte pool creation failed. tx_byte_pool_create() = 0x%02x\r\n", ret);
@@ -24,6 +24,11 @@ UINT BytePool::create(VOID *pool_start, ULONG pool_size) {
     return ret;
 }
 
+void BytePool::setBytePoolStruct(TX_BYTE_POOL *txBytePool) {
+    bytePool = txBytePool;
+}
+
+
 UCHAR *BytePool::allocate(const ULONG memory_size) {
     log(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
             ->printf("Stm32ThreadX::BytePool::allocate(%d)\r\n", memory_size);
@@ -31,7 +36,7 @@ UCHAR *BytePool::allocate(const ULONG memory_size) {
     UINT ret = TX_SUCCESS;
     UCHAR *memPtr = nullptr;
 
-    ret = tx_byte_allocate(this,
+    ret = tx_byte_allocate(bytePool,
                            reinterpret_cast<void **>(&memPtr),
                            memory_size,
                            TX_NO_WAIT);
--- src/BytePool.hpp
+++ src/BytePool.hpp
@@ -11,18 +11,31 @@
 #include "tx_api.h"
 
 namespace Stm32ThreadX {
-    class BytePool : public Stm32ItmLogger::Loggable, public TX_BYTE_POOL {
+    class BytePool : public Stm32ItmLogger::Loggable {
     public:
         BytePool() = default;
 
-        explicit BytePool(const TX_BYTE_POOL &other)
-            : TX_BYTE_POOL(other) {
+        explicit BytePool(TX_BYTE_POOL *txBytePool)
+            : bytePool(txBytePool) {
         }
 
         UINT create(VOID *pool_start, ULONG pool_size);
 
+        void setBytePoolStruct(TX_BYTE_POOL *txBytePool);
+
+        /**
+         * @brief Allocate memory from the BytePool.
+         *
+         * This method allocates memory of the specified size from the BytePool.
+         *
+         * @param memory_size The size of the memory to allocate.
+         *
+         * @return A pointer to the allocated memory. Returns nullptr if allocation fails.
+         */
         UCHAR *allocate(ULONG memory_size);
 
+    protected:
+        TX_BYTE_POOL *bytePool = {};
     };
 }
 #endif //LIBSMART_STM32THREADX_BYTEPOOL_HPP
